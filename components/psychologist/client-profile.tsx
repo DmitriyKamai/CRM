d1929daf0198db69d41a5120a9f3a7a9ff3a5d1e
Dmitriy Kamai <134800754+DmitriyKamai@users.noreply.github.com>
2026-03-12 02:57:50 +0300

    grip fix 3

--- components/psychologist/client-profile.tsx
+++ components/psychologist/client-profile.tsx
@@ -106,7 +106,7 @@ function SortableFieldWrap({
           <div className="absolute inset-0 flex items-stretch justify-center">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
+              viewBox="6 4 12 16"
               fill="none"
               stroke="currentColor"
               strokeWidth={2}
